f2bb3dde282f3b7d56d5ad86b5fa9f4d41951fca
diegocho <diegoalonzo2001@hotmail.com>
2022-10-08 19:54:17 -0500

    pago

---
+++ aplicacionraiz2022postgress/Data/Migrations/20220930035202_Initial1111123Migration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace aplicacionraiz2022postgress.Migrations
+{
+    public partial class Initial1111123Migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
